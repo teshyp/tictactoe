adb0bb870a6a3e986ebb85c3bb7fe79a61ee6c97
Tesh Patel <tesh.p@hotmail.co.uk>
2021-12-13 14:10:04 +0000

    Skeleton code

---
+++ app.js
@@ -0,0 +1,94 @@
+// Render gameboard object and create players
+
+const gameBoard = (() => {
+
+    const createPlayer = (name, marker) => {
+        return { name, marker };
+    }
+
+    // gererate gameboard with default image, set img data values, append img to gameboard
+    let gameSq = document.getElementsByClassName("gameSquare")
+    let board = [];
+
+    for (let i = 0;i < gameSq.length;i++) { //for each sq on the grid
+        const card = document.createElement("img") // create img
+        card.setAttribute("src", "images/white.png") // set img to blank img
+        card.setAttribute("data-value", i) // set data-value to NA
+        card.addEventListener("click", updateBoard)
+        gameSq[i].appendChild(card) // append img to gameSq Div
+        board.push(card.getAttribute("data-value")) // push data-value to board array
+    }
+    console.log("Board loaded " + board)
+    return board;
+}
+});
+
+
+
+function updateBoard() {
+    let player1Tracker = ["", "", "", "", "", "", "", "", ""];
+    let player2Tracker = ["", "", "", "", "", "", "", "", ""];
+
+
+    // update DOM board
+    // update gametrackers - store game
+    //
+    // player click -> has the img already been clicked? what is its value? If value is x - select another option
+    // store value in playerTracker
+    // change value of clicked img to X 
+    // if the playerTracker contains any of the winning combos -> alert winner
+    // remember clicked img
+
+
+    if (player1Tracker !== null) {
+        let clickedVal = this.getAttribute("data-value")
+        player1Tracker.push(clickedVal)
+        console.log(player1Tracker)
+        this.setAttribute("src", "images/o.jpeg")
+        return player1Tracker
+    } else {
+        let clickedVal = this.getAttribute("data-value")
+        player2Tracker.push(clickedVal)
+        console.log(player2Tracker)
+        this.setAttribute("src", "images/x.jpeg")
+        return player2Tracker
+    }
+}
+
+
+gameBoard()
+
+// const operateGame = (() => {
+
+//     // declare players
+
+//     // check winner function
+//     function checkWinner(player1Score, player2Score) {
+//     }
+
+//     const winningAxes = [
+//         [0, 1, 2],
+//         [3, 4, 5],
+//         [6, 7, 8],
+//         [0, 3, 6],
+//         [1, 4, 7],
+//         [2, 5, 8],
+//         [0, 4, 8],
+//         [2, 4, 6],
+//     ];
+
+// }
+
+
+
+// returning functions: check for winner, next player
+
+// Build the functions 
+//  that allow players to add marks to a specific spot on the board
+//  and then tie it to the DOM - letting players click on the gameboard to place their marker.
+// Don’t forget the logic that keeps players from playing in spots that are already taken!
+// Think carefully about where each bit of logic should reside.
+// Each little piece of functionality should be able to fit in:-
+// the game, player or gameboard objects
+//..but take care to put them in “logical” places.
+// Spending a little time brainstorming here can make your life much easier later!
